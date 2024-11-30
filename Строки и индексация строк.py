cab49553b90f67aff94912dec7edc1e63368d2dd
ZagalovM <zagalov777@gmail.com>
2024-11-30 14:29:48 +0300

    Add files via upload
    Практическое задание по уроку "Строки и индексация строк"

---
+++ Строки и индексация строк.py
@@ -0,0 +1,16 @@
+example = "Auris"
+print(example[0])
+
+example = "Auris"
+print(example[-1])
+
+example = "Auris"
+print(example[2:5])
+
+example = "Auris"
+print(example[::-1])
+
+example = "Auris"
+print(example[1:5:2])
+
+
